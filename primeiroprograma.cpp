102bee3868a2e0d83bfbf9f2e8bdb0ca0dcd101e
largado <Kv2940bm@>
2022-03-10 22:33:01 -0300

    first commit

---
+++ primeiroprograma.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+const int NUMERO_SECRETO = 42;
+
+bool nao_acertou = true;
+int tentativas = 0;
+
+while(nao_acertou)
+{
+    tentativas++:
+    int chute;
+    cout << "Tentativas " << tentativas << endl;
+    bool acertou = chute == NUMERO_SECRETO;
+    bool maior = chute > NUMERO_SECRETO;
+}
+
+
+int main() {
+    cout << "Bem vindo ao jogo da adivinhação" << endl;
+
+    int numero = 43;
+
+    cout << "imprime a variável : " << numero << endl;
+}
+
+
